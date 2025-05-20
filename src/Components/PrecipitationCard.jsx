805214f56b83b75dcad16280aa8b9228a3ce6bf6
dahl898 <wdmakota@gmail.com>
2025-05-20 22:08:43 +0200

    Change precipitation card styling to match other cards

--- src/Components/PrecipitationCard.jsx
+++ src/Components/PrecipitationCard.jsx
@@ -5,29 +5,34 @@ import PrecipitationSVG from '../SVG/PrecipitationSVG'
 import DewPointSVG from '../SVG/DewPointSVG'
 import FeelsSVG from '../SVG/FeelsSVG'
 
-export default function PrecipitationCard({ temperature }) {
+export default function PrecipitationCard({ temperature, width, height }) {
   return (
     <>
-      <div className={style.card}>
+      <div className={style.card} style={{ width: width, height: height }}>
         <p className={style.type}>Precipitation</p>
         <div className={style.precipitation_container}>
-          <div className={style.precipitation_icon_container}>
-            <PrecipitationSVG style={style} />
+          <div className={style.precipitation_headline_container}>
+            <div className={style.precipitation_icon_container}>
+              <PrecipitationSVG style={style} />
+            </div>
+            <h1 className={style.precipitation_level}>22 mm</h1>
           </div>
-          <h1 className={style.precipitation_level}>22 mm</h1>
-        </div>
-        <div className={style.precipitation_general_data_container}>
-          <div className={`${style.data_container} ${style.one}`}>
-            <UmbrellaSVG style={style} />
-            <span className={style.span}>23%</span>
-            <span className={style.span}>Chances</span>
-          </div>
-          <div className={`${style.data_container} ${style.two}`}>
-            <HumiditySVG style={style} />
-            <span className={style.span}>46%</span>
-            <span className={style.span}>Humidity</span>
-          </div>
-          <div className={`${style.data_container} ${style.three}`}>
+          <div className={style.precipitation_general_data_container}>
+            <div className={`${style.data_container} ${style.one}`}>
+              <UmbrellaSVG style={style} />
+              <div className={style.span_container}>
+                <span className={style.span}>23%</span>
+                <span className={style.span}>Chances</span>
+              </div>
+            </div>
+            <div className={`${style.data_container} ${style.one}`}>
+              <HumiditySVG style={style} />
+              <div className={style.span_container}>
+                <span className={style.span}>23%</span>
+                <span className={style.span}>Chances</span>
+              </div>
+            </div>
+            {/* <div className={`${style.data_container} ${style.three}`}>
             <DewPointSVG style={style} />
             <span className={style.span}>14"</span>
             <span className={style.span}>Dew point</span>
@@ -36,6 +41,7 @@ export default function PrecipitationCard({ temperature }) {
             <FeelsSVG style={style} />
             <span className={style.span}>Sauna</span>
             <span className={style.span}>Feels</span>
+          </div> */}
           </div>
         </div>
       </div>
