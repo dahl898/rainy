dafcfd5e9303c2dda1910612caa441ca7fd07225
dahl898 <wdmakota@gmail.com>
2025-05-20 22:06:10 +0200

    Add css module for Scroll component. Change scroll orientation to vertical

--- src/Components/Scroll.jsx
+++ src/Components/Scroll.jsx
@@ -1,13 +1,25 @@
 import PrecipitationCard from './PrecipitationCard'
 import TemperatureCard from './TemperatureCard'
 import PrecipitationLevelCard from './PrecipitationLevelCard'
+import style from './Scroll.module.css'
+import { useState } from 'react'
 
 export default function Scroll({ forecastObject }) {
+  console.log(forecastObject.daily.values.precipitationIntensity)
+  const [width, setWidth] = useState('350px')
+  const [height, setHeight] = useState('230px')
+
   return (
-    <>
-      <PrecipitationCard temperature={23} />
-      <TemperatureCard forecastObject={forecastObject} />
-      <PrecipitationLevelCard />
-    </>
+    <div className={style.scroll_container}>
+      <PrecipitationCard temperature={23} width={width} height={height} />
+      <TemperatureCard
+        forecastObject={forecastObject}
+        width={width}
+        height={height}
+      />
+      {forecastObject.daily.values.precipitationIntensity !== 0 && (
+        <PrecipitationLevelCard width={width} height={height} />
+      )}
+    </div>
   )
 }
